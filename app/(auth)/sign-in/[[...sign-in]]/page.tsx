707634da1aa3122b3b709ee3d78a98811096e2ac
ianh8899 <ihitchman83@gmail.com>
2023-11-29 17:28:34 +0000

    Updating Clerk Positioning

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <div className="flex justify-center align-middle mt-4">
+  return <div className="flex justify-center align-middle mt-12">
     <SignIn />;
   </div>
 }
